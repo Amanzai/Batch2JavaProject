712cf682f07e9faf9daf2641587f35d4de51686d
Samiullah.Amanzai <Samiullah.Amanzai1361@gmail.com>
2023-09-04 11:46:52 -0400

    first commit

---
+++ src/class14/Methodpractice.java
@@ -0,0 +1,18 @@
+package class14;
+
+public class Methodpractice {
+
+    int num=10;
+    int num1=5;
+    public void ridTwoNumbers(){
+        System.out.println(" The multiplication of two number is "+(num-num1));
+    }
+
+    public static void main(String[] args) {
+
+        Methodpractice obj=new Methodpractice();
+        obj.ridTwoNumbers();
+
+    }
+
+}
---
+++ src/class18/AccessModifiers.java
@@ -0,0 +1,11 @@
+package class18;
+
+public class AccessModifiers {
+    public String name;
+    private String AccountPassword;
+    String SSNNumber;
+    public void park(){
+        System.out.println("everyone can has access to park");
+    }
+
+}
